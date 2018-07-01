176303c97c8c2d7b742fc6f1bbee8fc3cc22c239
Joseph Callen <jcallen@redhat.com>
2018-07-01 19:02:20 -0400

    ocp install

--- deploy/ocp/preq.sh
+++ deploy/ocp/preq.sh
@@ -2,11 +2,31 @@
 
 set -x
 
-sudo yum install epel-release -y
-sudo yum install vim curl wget docker -y
+subscription-manager repos --disable="*"
 
+subscription-manager config --rhsm.auto_enable_yum_plugins=0
 
-sudo tee /etc/sysconfig/docker-storage-setup << EOF
+subscription-manager register --username='jcallen@redhat.com'
+
+subscription-manager attach --pool=8a85f98260c27fc50160c328c08a5045
+
+subscription-manager repos \
+    --enable="rhel-7-server-rpms" \
+    --enable="rhel-7-server-extras-rpms" \
+    --enable="rhel-7-server-ose-3.9-rpms" \
+    --enable="rhel-7-fast-datapath-rpms" \
+    --enable="rhel-7-server-ansible-2.4-rpms"
+
+yum update -y
+yum install vim curl wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct -y
+
+yum install docker-1.13.1 openshift-ansible -y
+
+tee -a /etc/sysconfig/docker << EOF
+INSECURE_REGISTRY='--insecure-registry 172.30.0.0/16'
+EOF
+
+tee /etc/sysconfig/docker-storage-setup << EOF
 STORAGE_DRIVER=overlay2
 DEVS=/dev/nvme0n1
 CONTAINER_ROOT_LV_NAME=dockerlv
@@ -15,6 +35,6 @@ VG=dockervg
 EOF
 
 
-sudo systemctl start docker
+systemctl start docker
 
 
